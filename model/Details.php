f03f6dfc3e7f180e3aea4f3669d9807ee1a99475
Ubuntu <ubuntu@ip-172-31-3-162.ap-southeast-2.compute.internal>
2015-08-05 06:31:18 +0000

    Account Details functionality finished except Interest Calculations

--- model/Details.php
+++ model/Details.php
@@ -13,34 +13,29 @@ class Details {
 	
 	public function init(){
 		
-		if(!isset($_SESSION['accounts'])){
-			$_SESSION['accounts'] = array();
-			$_SESSION['accounts'][] = array('accountID' => '1',
-					'accountName' => 'Kinkead Family Trust/083-006 45-333-3232'
-			);
-			$_SESSION['accounts'][] = array('accountID' => '2',
-					'accountName' => 'Kinkead Murphy Unit Trust/083-006 45-214-8745'
-			);
-			$_SESSION['accounts'][] = array('accountID' => '3',
-					'accountName' => 'Kinkead Superannuation Fund/083-006 45-546-3298'
-			);
-		}
+		$accounts = new Accounts();
+		$accounts->userID = $_SESSION['userID'];
+		$_SESSION['accounts'] = $accounts->getAccounts();
 		
-		if(isset($_SESSION['detAccountID'])){
-			$this->setAccountSelected($_SESSION['detAccountID']);
-		}
+		$firstAccount = $accounts->getFirstAccount();
+		$_SESSION['detAccountID'] = $firstAccount['accountID'];
+		$this->setAccountSelected($_SESSION['detAccountID']);
+		
+		$account = new Account();
+		$account->accountID =  $firstAccount['accountID'];
+		$account->getAccount();
 		
 		if(!isset($_SESSION['detAccountNickname'])){
-			$_SESSION['detAccountNickname'] = 'Kinkead Family Trust';
+			$_SESSION['detAccountNickname'] = $account->accountNickname;
 		}
 		if(!isset($_SESSION['detAccountNumber'])){
-			$_SESSION['detAccountNumber'] = '89-445-3454';
+			$_SESSION['detAccountNumber'] = $account->accountNumber;
 		}
 		if(!isset($_SESSION['detProductName'])){
-			$_SESSION['detProductName'] = 'Personal Cheque Account';
+			$_SESSION['detProductName'] = $account->productName;
 		}
 		if(!isset($_SESSION['detRecordedLimit'])){
-			$_SESSION['detRecordedLimit'] = '0.00';
+			$_SESSION['detRecordedLimit'] = $account->recordedLimit;
 		}
 		if(!isset($_SESSION['detAccruedDebitInterest'])){
 			$_SESSION['detAccruedDebitInterest'] = '0.00';
@@ -66,35 +61,22 @@ class Details {
 	
 	public function getDetails($accountID){
 		
-		if(!isset($_SESSION['accounts'])){
-			$_SESSION['accounts'] = array();
-			$_SESSION['accounts'][] = array('accountID' => '1',
-					'accountName' => 'Kinkead Family Trust/083-006 45-333-3232'
-			);
-			$_SESSION['accounts'][] = array('accountID' => '2',
-					'accountName' => 'Kinkead Murphy Unit Trust/083-006 45-214-8745'
-			);
-			$_SESSION['accounts'][] = array('accountID' => '3',
-					'accountName' => 'Kinkead Superannuation Fund/083-006 45-546-3298'
-			);
-		}
+		$accounts = new Accounts();
+		$accounts->userID = $_SESSION['userID'];
+		$_SESSION['accounts'] = $accounts->getAccounts();
 		
 		$_SESSION['detAccountID'] = $accountID;
 		
 		$this->setAccountSelected($_SESSION['detAccountID']);
 		
-		if(!isset($_SESSION['detAccountNickname'])){
-			$_SESSION['detAccountNickname'] = 'Kinkead Other Account';
-		}
-		if(!isset($_SESSION['detAccountNumber'])){
-			$_SESSION['detAccountNumber'] = '43-446-3421';
-		}
-		if(!isset($_SESSION['detProductName'])){
-			$_SESSION['detProductName'] = 'Overdraft Account';
-		}
-		if(!isset($_SESSION['detRecordedLimit'])){
-			$_SESSION['detRecordedLimit'] = '5000 DR';
-		}
+		$account = new Account();
+		$account->accountID =  $accountID;
+		$account->getAccount();
+		
+		$_SESSION['detAccountNickname'] = $account->accountNickname;
+		$_SESSION['detAccountNumber'] = $account->accountNumber;
+		$_SESSION['detProductName'] = $account->productName;
+		$_SESSION['detRecordedLimit'] = $account->recordedLimit;
 		if(!isset($_SESSION['detAccruedDebitInterest'])){
 			$_SESSION['detAccruedDebitInterest'] = '3.00';
 		}
