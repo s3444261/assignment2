046e0ebfa62631897b87bd88834ee5da93fffe3b
Ubuntu <ubuntu@ip-172-31-3-162.ap-southeast-2.compute.internal>
2015-08-09 11:37:14 +0000

    Payee List functionality completed

--- model/BillerPayees.php
+++ model/BillerPayees.php
@@ -98,5 +98,106 @@ class BillerPayees
     	 
     	return $payees;
     }
+    
+    public function getBillersList()
+    {
+    	$query = "SELECT billerNickname AS payeeNickname,
+    				billerName AS payeeName,
+    				'Biller' AS payeeType,
+    				CONCAT('<a href=\"Biller-Modify/', billerID, '\">Modify</a>') AS payeeModify,
+    				CONCAT('<a href=\"Biller-Delete/', billerID, '\">Delete</a>') AS payeeDelete
+    				FROM Billers
+    				WHERE userID = :userID
+    				AND billerStatus != 'deleted'
+    				ORDER BY payeeNickname ASC";
+    	 
+    	$db = Database::getInstance();
+    	$stmt = $db->prepare($query);
+    	$stmt->bindParam(':userID', $this->_userID);
+    	$stmt->execute();
+    	
+    	$billersList = array();
+    	while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+    		$biller = array('payeeNickname' => $row['payeeNickname'],
+					'payeeName' =>  $row['payeeName'],
+					'payeeType' => $row['payeeType'],
+					'payeeModify' => $row['payeeModify'],
+					'payeeDelete' => $row['payeeDelete']
+    				);
+    		$billers[] = $biller;
+    	}
+    	return $billers;
+    }
+    
+    public function getPayeesList()
+    {
+    	$query = "SELECT accountNickname AS payeeNickname,
+    				accountName AS payeeName,
+    				'Payee' AS payeeType,
+    				CONCAT('<a href=\"Payee-Modify/', payeeID, '\">Modify</a>') AS payeeModify,
+    				CONCAT('<a href=\"Payee-Delete/', payeeID, '\">Delete</a>') AS payeeDelete
+    				FROM Payees
+    				WHERE userID = :userID
+    				AND payeeStatus != 'deleted'
+    				ORDER BY payeeNickname ASC";
+    
+    	$db = Database::getInstance();
+    	$stmt = $db->prepare($query);
+    	$stmt->bindParam(':userID', $this->_userID);
+    	$stmt->execute();
+    	 
+    	$payeesList = array();
+    	while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+    		$payee = array('payeeNickname' => $row['payeeNickname'],
+					'payeeName' =>  $row['payeeName'],
+					'payeeType' => $row['payeeType'],
+					'payeeModify' => $row['payeeModify'],
+					'payeeDelete' => $row['payeeDelete']
+    				);
+    		$payeesList[] = $payee;
+    	}
+    
+    	return $payeesList;
+    }
+    
+    public function getBothList()
+    {
+    	$query = "SELECT billerNickname AS payeeNickname,
+    				billerName AS payeeName,
+    				'Biller' AS payeeType,
+    				CONCAT('<a href=\"Biller-Modify/', billerID, '\">Modify</a>') AS payeeModify,
+    				CONCAT('<a href=\"Biller-Delete/', billerID, '\">Delete</a>') AS payeeDelete
+    				FROM Billers
+    				WHERE userID = :userID
+    				AND billerStatus != 'deleted'
+    			UNION
+    			SELECT accountNickname AS payeeNickname,
+    				accountName AS payeeName,
+    				'Payee' AS payeeType,
+    				CONCAT('<a href=\"Payee-Modify/', payeeID, '\">Modify</a>') AS payeeModify,
+    				CONCAT('<a href=\"Payee-Delete/', payeeID, '\">Delete</a>') AS payeeDelete
+    				FROM Payees
+    				WHERE userID = :userID
+    				AND payeeStatus != 'deleted'
+    			ORDER BY payeeNickname ASC";
+    
+    	$db = Database::getInstance();
+    	$stmt = $db->prepare($query);
+    	$stmt->bindParam(':userID', $this->_userID);
+    	$stmt->execute();
+    
+    	$bothList = array();
+    	while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+    		$both = array('payeeNickname' => $row['payeeNickname'],
+					'payeeName' =>  $row['payeeName'],
+					'payeeType' => $row['payeeType'],
+					'payeeModify' => $row['payeeModify'],
+					'payeeDelete' => $row['payeeDelete']
+    				);
+    		$bothList[] = $both;
+    	}
+    
+    	return $bothList;
+    }
 }
 ?>
--- model/Payeelist.php
+++ model/Payeelist.php
@@ -16,83 +16,17 @@ class Payeelist {
 		$payment->cancelSessions();
 		
 		if(isset($_SESSION['billPayee']) || isset($_SESSION['billPayeeList'])){
-			$_SESSION ['payeeList'] = array();
-			$_SESSION ['payeeList'][] = array('payeeNickname' => 'ASIC',
-					'payeeName' => 'AUSTRALIAN SECURITIES & INVESTMENTS COMMISSION',
-					'payeeType' => 'Biller',
-					'payeeModify' => '<a href="Biller-Modify/1">Modify</a>',
-					'payeeDelete' => '<a href="Biller-Delete/1">Delete</a>'
-			);
-			$_SESSION ['payeeList'][] = array('payeeNickname' => 'ATO',
-					'payeeName' => 'AUSTRALIAN TAXATION OFFICE',
-					'payeeType' => 'Biller',
-					'payeeModify' => '<a href="Biller-Modify/2">Modify</a>',
-					'payeeDelete' => '<a href="Biller-Delete/2">Delete</a>'
-			);
-			$_SESSION ['payeeList'][] = array('payeeNickname' => 'GIO',
-					'payeeName' => 'AAI LIMITED T/AS GIO HOME & MOTOR INSURANCE',
-					'payeeType' => 'Biller',
-					'payeeModify' => '<a href="Biller-Modify/3">Modify</a>',
-					'payeeDelete' => '<a href="Biller-Delete/3">Delete</a>'
-			);
+			$billers = new BillerPayees();
+			$billers->userID = $_SESSION['userID'];
+			$_SESSION ['payeeList'] = $billers->getBillersList();
 		} elseif(isset($_SESSION['fundsTransferPayee']) || isset($_SESSION['fundsTransferPayeeList'])){
-			$_SESSION ['payeeList'] = array();
-			$_SESSION ['payeeList'][] = array('payeeNickname' => 'Trust Fund',
-					'payeeName' => 'Kinkead Family Trust/BSB 083-334 Acct 87-345-4455',
-					'payeeType' => 'Payee',
-					'payeeModify' => '<a href="Payee-Modify/1">Modify</a>',
-					'payeeDelete' => '<a href="Payee-Delete/1">Delete</a>'
-			);
-			$_SESSION ['payeeList'][] = array('payeeNickname' => 'Super Fund',
-					'payeeName' => 'Kinkead Superannuation Fund/BSB 083-334 Acct 86-345-9998',
-					'payeeType' => 'Payee',
-					'payeeModify' => '<a href="Payee-Modify/1">Modify</a>',
-					'payeeDelete' => '<a href="Payee-Delete/1">Delete</a>'
-			);
-			$_SESSION ['payeeList'][] = array('payeeNickname' => 'Cheque Account',
-					'payeeName' => 'G. Kinkead Cheque Account/BSB 083-334 Acct 87-331-8877',
-					'payeeType' => 'Payee',
-					'payeeModify' => '<a href="Payee-Modify/1">Modify</a>',
-					'payeeDelete' => '<a href="Payee-Delete/1">Delete</a>'
-			);
+			$payees = new BillerPayees();
+			$payees->userID = $_SESSION['userID'];
+			$_SESSION ['payeeList'] = $payees->getPayeesList();
 		} elseif(isset($_SESSION['allPayeeList'])){
-			$_SESSION ['payeeList'] = array();
-			$_SESSION ['payeeList'][] = array('payeeNickname' => 'Trust Fund',
-					'payeeName' => 'Kinkead Family Trust/BSB 083-334 Acct 87-345-4455',
-					'payeeType' => 'Payee',
-					'payeeModify' => '<a href="Payee-Modify/1">Modify</a>',
-					'payeeDelete' => '<a href="Payee-Delete/1">Delete</a>'
-			);
-			$_SESSION ['payeeList'][] = array('payeeNickname' => 'ASIC',
-					'payeeName' => 'AUSTRALIAN SECURITIES & INVESTMENTS COMMISSION',
-					'payeeType' => 'Biller',
-					'payeeModify' => '<a href="Biller-Modify/1">Modify</a>',
-					'payeeDelete' => '<a href="Biller-Delete/1">Delete</a>'
-			);
-			$_SESSION ['payeeList'][] = array('payeeNickname' => 'ATO',
-					'payeeName' => 'AUSTRALIAN TAXATION OFFICE',
-					'payeeType' => 'Biller',
-					'payeeModify' => '<a href="Biller-Modify/2">Modify</a>',
-					'payeeDelete' => '<a href="Biller-Delete/2">Delete</a>'
-			);
-			$_SESSION ['payeeList'][] = array('payeeNickname' => 'Super Fund',
-					'payeeName' => 'Kinkead Superannuation Fund/BSB 083-334 Acct 86-345-9998',
-					'payeeType' => 'Payee',
-					'payeeModify' => '<a href="Payee-Modify/1">Modify</a>',
-					'payeeDelete' => '<a href="Payee-Delete/1">Delete</a>'
-			);
-			$_SESSION ['payeeList'][] = array('payeeNickname' => 'Cheque Account',
-					'payeeName' => 'G. Kinkead Cheque Account/BSB 083-334 Acct 87-331-8877',
-					'payeeType' => 'Payee',
-					'payeeModify' => '<a href="Payee-Modify/1">Modify</a>',
-					'payeeDelete' => '<a href="Payee-Delete/1">Delete</a>'
-			);
-			$_SESSION ['payeeList'][] = array('payeeNickname' => 'GIO',
-					'payeeName' => 'AAI LIMITED T/AS GIO HOME & MOTOR INSURANCE',
-					'payeeType' => 'Biller',
-					'payeeModify' => '<a href="Biller-Modify/3">Modify</a>',
-					'payeeDelete' => '<a href="Biller-Delete/3">Delete</a>'
-			);
+			$both = new BillerPayees();
+			$both->userID = $_SESSION['userID'];
+			$_SESSION ['payeeList'] = $both->getBothList();
 		}
 	}
 }
